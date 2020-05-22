b591b67e0130f3ef085ab001f2faa1ef583cd32e
Vincenzo Guida <enzoguida1997@gmail.com>
2020-05-22 18:35:26 +0200

    Front end

---
+++ Fifo Queue/Fifo/Views/AddView.swift
@@ -0,0 +1,21 @@
+//
+//  AddView.swift
+//  Fifo
+//
+//  Created by Vincenzo Guida on 20/05/2020.
+//  Copyright © 2020 GueTeam. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
